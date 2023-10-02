317f71a3e1b19550a3bcb29b9d6605a850d4a316
Dina-S-A <87444987+Dina-S-A@users.noreply.github.com>
2023-10-02 19:09:52 +0000

    initializing GitHub

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const test = "Test";
 app.set('view engine', 'ejs');
 app.get('/', (req, res) => {
     res.render('index', { title: 'My Express App' });
